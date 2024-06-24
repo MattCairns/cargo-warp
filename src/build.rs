82ec623baa6ec73c87326cb80dd6c654759310fa
Matthew Cairns <git@cairns.pro>
2024-06-24 15:29:43 -0700

    feat: Add support for cross

--- src/build.rs
+++ src/build.rs
@@ -1,7 +1,12 @@
 use cargo_metadata::Message;
 use std::process::{Command, Stdio};
 
-pub fn cargo_build(project: Option<&str>) -> Vec<std::path::PathBuf> {
+pub enum BuildType {
+    Cargo,
+    Cross,
+}
+
+pub fn cargo_build(project: Option<&str>, target: Option<&str>, build_type: BuildType) -> Vec<std::path::PathBuf> {
     let mut args: Vec<&str> = vec!["build", "--message-format=json-render-diagnostics"];
 
     if let Some(project) = project {
@@ -9,11 +14,19 @@ pub fn cargo_build(project: Option<&str>) -> Vec<std::path::PathBuf> {
         args.push(project)
     }
 
-    let mut command = Command::new("cargo")
-        .args(args)
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
+    if let Some(target) = target {
+        args.push("-t");
+        args.push(target)
+    }
+
+    let mut command = Command::new(match build_type {
+        BuildType::Cargo => "cargo",
+        BuildType::Cross => "cross",
+    })
+    .args(args)
+    .stdout(Stdio::piped())
+    .spawn()
+    .unwrap();
 
     let reader = std::io::BufReader::new(command.stdout.take().unwrap());
     let mut files: Vec<std::path::PathBuf> = vec![];
